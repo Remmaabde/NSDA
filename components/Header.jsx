1e4dcf87d82478d36a120b14a5dad8f6a400b7cb
Reema_abde <Reemabdella702@gmail.com>
2025-08-06 20:30:54 +0300

    Islamic To-Do App Week-1

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <header className="navbar">
+      <nav className="nav-links">
+        <a href="#">Home</a>
+        <a href="#">Tasks</a>
+        <a href="#">Explore</a>
+        <a href="#">About</a>
+      </nav>
+    </header>
+    )
+}
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import {Home} from '../components/Home'
+import Header from '../components/Header'
+
+function App() {
+  return(
+  <>
+  <Home/>
+  <Header/>
+  
+
+  </>
+  )
+}
+
+
+export default App
